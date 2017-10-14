0b157c2a3676c2cb807a2d7a5aa37c3752c888cb
nbfujx <nbfujx@qq.com>
2017-10-14 21:54:39 +0800

    20171014 增加dubbo相关配置和启动

---
+++ Goku.WebService.Provider/src/main/java/com/goku/provider/LuncherProvider.java
@@ -0,0 +1,10 @@
+package com.goku.provider;
+
+/**
+ * Created by nbfujx on 2017/10/14.
+ */
+public class LuncherProvider {
+    public static void main(String[] args) {
+        com.alibaba.dubbo.container.Main.main(args);
+    }
+}
--- Goku.WebService.Provider/src/main/java/com/goku/service/impl/sysUserInfoServiceImpl.java
+++ Goku.WebService.Provider/src/main/java/com/goku/service/impl/sysUserInfoServiceImpl.java
@@ -1,10 +1,12 @@
 package com.goku.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.goku.service.sysUserInfoService;
 
 /**
  * Created by nbfujx on 2017/10/14.
  */
+@Service
 public class sysUserInfoServiceImpl implements sysUserInfoService {
 
 }
