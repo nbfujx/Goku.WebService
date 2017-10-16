a4f9c874ed08582300b5d542778b53f037308fdc
nbfujx <nbfujx@qq.com>
2017-10-16 21:45:10 +0800

    20171016 增加swagger配置

--- Goku.WebService.Webapi/src/main/java/com/goku/webapi/controller/impl/sysUserControllerImpl.java
+++ Goku.WebService.Webapi/src/main/java/com/goku/webapi/controller/impl/sysUserControllerImpl.java
@@ -2,6 +2,8 @@ package com.goku.webapi.controller.impl;
 
 import com.goku.webapi.controller.sysUserController;
 import com.goku.webapi.handler.sysUserHandler;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("sysUser")
+@Api(value="sysUser")
 public class sysUserControllerImpl implements sysUserController {
 
     @Autowired
@@ -23,6 +26,7 @@ public class sysUserControllerImpl implements sysUserController {
 
     @Override
     @RequestMapping(value="getUserPassword", method = RequestMethod.GET)
+    @ApiOperation(value = "获取用户名,密码", httpMethod = "GET", response = sysUserController.class, notes = "获取用户名,密码")
     public List<Map<String, String>> getUserPassword(){//(@PathVariable String orgid, @PathVariable Integer pageNo, @PathVariable Integer pageSize,
                                                        //@PathVariable String orderSort, @PathVariable String orderFiled) {
         return   sysuserHandler.selectUserByOrgid("1",1,10,"desc","id");//sysuserHandler.selectUserByOrgid(orgid,pageNo,pageSize,orderSort,orderFiled);
