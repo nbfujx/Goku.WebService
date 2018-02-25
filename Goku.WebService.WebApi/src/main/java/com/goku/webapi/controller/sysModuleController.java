9aa9cc01faa917b9851e6da1f1192d04f84c7ac0
nbfujx <nbfujx@qq.com>
2018-02-25 19:02:41 +0800

    删除

--- Goku.WebService.WebApi/src/main/java/com/goku/webapi/controller/sysModuleController.java
+++
@@ -1,8 +0,0 @@
-package com.goku.webapi.controller;
-
-/**
- * Created by nbfujx on 2018/2/11.
- */
-public interface SysModuleController {
-    String getModuleForPaging();
-}
--- Goku.WebService.WebApi/src/main/java/com/goku/webapi/handler/sysModuleHandle.java
+++
@@ -1,9 +0,0 @@
-package com.goku.webapi.handler;
-
-/**
- * Created by nbfujx on 2018/2/11.
- */
-public interface SysModuleHandle {
-    String selectByid(String id);
-    String getModuleForPaging();
-}
