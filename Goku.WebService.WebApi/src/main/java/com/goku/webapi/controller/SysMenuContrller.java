e5a9a28d0148d13eb4711f5eca43e7924bfafb94
nbfujx <nbfujx@qq.com>
2018-02-25 18:58:38 +0800

    删除目录，重新整理

--- Goku.WebService.WebApi/src/main/java/com/goku/webapi/controller/SysMenuContrller.java
+++
@@ -1,7 +0,0 @@
-package com.goku.webapi.controller;
-
-/**
- * Created by nbfujx on 2018/2/23.
- */
-public interface SysMenuContrller {
-}
--- Goku.WebService.WebApi/src/main/java/com/goku/webapi/controller/impl/SysMenuContrllerImpl.java
+++
@@ -1,9 +0,0 @@
-package com.goku.webapi.controller.impl;
-
-import com.goku.webapi.controller.SysMenuContrller;
-
-/**
- * Created by nbfujx on 2018/2/23.
- */
-public class SysMenuContrllerImpl implements SysMenuContrller {
-}
